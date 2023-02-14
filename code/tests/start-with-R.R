d87476f3d6e5e89f9508888ef6f2e6d6fa6fd5cc
Atum Scott <atumscot@gmail.com>
2023-02-14 10:08:23 +0000

    Add tests

---
+++ code/tests/start-with-R.R
@@ -0,0 +1,4 @@
+list(plans = c("fastest", "balanced", "quietest", "ebike"),
+     plans = c("fastest"),
+     min_flow = 430, # Set to 1 for full build, set to high value (e.g. 400) for tests
+     date_routing = "2023-02-14")
---
+++ code/tests/test-batch.R
@@ -0,0 +1,94 @@
+
+library(targets)
+library(sf)
+
+# Aim: test batch routing
+Sys.getenv("CYCLESTREETS_PW")
+Sys.getenv("CYCLESTREETS_BATCH")
+Sys.getenv("CYCLESTREETS_URL")
+
+tar_load(od_commute_subset)
+
+od_test_100 = od_commute_subset[1:100, ]
+od_test_100$id = seq(nrow(od_test_100))
+sf::write_sf(od_test_100, "od_test_100.geojson", delete_dsn = TRUE)
+
+Sys.time({
+batch_test_100 = cyclestreets::batch(
+  desire_lines = od_test_100,
+  serverId = "27",
+  strategies = "fastest",
+  username = "robinlovelace",
+  password = Sys.getenv("CYCLESTREETS_PW"),
+  pat = Sys.getenv("CYCLESTREETS_BATCH"),
+  silent = FALSE
+)
+})
+
+Sys.time(
+  {
+  route_test_100 <- router::route(desire_lines = od_test_100, route_fun = cyclestreets::journey, pat = "ca43ed677e5e6fe7")
+}
+)
+100 / 15
+
+od_test_1000 = od_commute_subset[1:1000, ]
+
+Sys.time({
+  batch_test_1000 = cyclestreets::batch(
+    desire_lines = od_test_1000,
+    serverId = "27",
+    strategies = "fastest",
+    username = "robinlovelace",
+    password = Sys.getenv("CYCLESTREETS_PW"),
+    pat = Sys.getenv("CYCLESTREETS_BATCH"),
+    silent = FALSE
+  )
+})
+
+od_test_2000 = od_commute_subset[1:2000, ]
+system.time({
+  batch_test_2000 <- cyclestreets::batch(
+    desire_lines = od_test_2000,
+    serverId = "27",
+    strategies = "fastest",
+    username = "robinlovelace",
+    password = Sys.getenv("CYCLESTREETS_PW"),
+    pat = Sys.getenv("CYCLESTREETS_BATCH"),
+    silent = FALSE
+  )
+})
+# 2000 routes, 254s, 7.9 routes/s
+# user  system elapsed 
+# 23.267   1.905 253.837 
+
+
+od_test_10000 = od_commute_subset[1:10000, ]
+system.time({
+  batch_test_10000 <- cyclestreets::batch(
+    desire_lines = od_test_10000,
+    serverId = "27",
+    strategies = "fastest",
+    username = "robinlovelace",
+    password = Sys.getenv("CYCLESTREETS_PW"),
+    pat = Sys.getenv("CYCLESTREETS_BATCH"),
+    silent = FALSE
+  )
+})
+# 2000 routes, 254s, 7.9 routes/s
+# user  system elapsed 
+# 23.267   1.905 253.837 
+
+od_test_10000 = od_commute_subset[1:10000, ]
+
+system.time({
+  batch_test_all <- cyclestreets::batch(
+    desire_lines = od_commute_subset,
+    serverId = "27",
+    strategies = "balanced",
+    username = "robinlovelace",
+    password = Sys.getenv("CYCLESTREETS_PW"),
+    pat = Sys.getenv("CYCLESTREETS_BATCH"),
+    silent = FALSE
+  )
+})
