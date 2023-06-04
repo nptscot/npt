c0051a03ce42a44ae9750fcd0e3ef1aeb4c525c6
atumscott <atumscot@gmail.com>
2023-06-04 23:13:01 +0100

    Step towards combined net #147

--- _targets.R
+++ _targets.R
@@ -303,11 +303,54 @@ list(
     rnet$total_cyclists_segment = rowSums(rnet_bicycle)
     
     names(rnet)[1:8] = paste0("commute_", names(rnet))[1:8]
-    rnet = rnet %>% 
+    rnet_commute = rnet %>% 
       filter(total_cyclists_segment > 0) %>% 
       select(-total_cyclists_segment) %>% 
       as.data.frame() %>% 
       sf::st_as_sf()
+    
+    # Create rnet_school
+    rnet_long = list(rnet_school, rnet_commute)
+    
+    rnet_long = data.table::rbindlist(rnet_long, fill = TRUE)
+    rnet_long = rnet_long[,c(1:8,10:19,9)]
+    rnet_long = rnet_long %>% 
+      mutate(across(school_fastest_bicycle:commute_ebike_bicycle_go_dutch, function(x) tidyr::replace_na(x, 0))) %>% 
+      as_tibble()
+    
+    rnet_long$geometry = sf::st_sfc(rnet_long$geometry, recompute_bbox = TRUE)
+    rnet_long = sf::st_as_sf(rnet_long)
+    
+    rnet_combined = overline(rnet_long, 
+                             attrib = c("school_fastest_bicycle","school_fastest_bicycle_go_dutch","school_quietest_bicycle",
+                                        "school_quietest_bicycle_go_dutch","school_balanced_bicycle","school_balanced_bicycle_go_dutch",
+                                        "school_ebike_bicycle","school_ebike_bicycle_go_dutch",                 
+                                        "Gradient","Quietness","commute_fastest_bicycle",
+                                        "commute_fastest_bicycle_go_dutch","commute_balanced_bicycle","commute_balanced_bicycle_go_dutch",
+                                        "commute_quietest_bicycle","commute_quietest_bicycle_go_dutch","commute_ebike_bicycle",
+                                        "commute_ebike_bicycle_go_dutch"), 
+                             fun = list(sum = sum, max = max),
+                             regionalise = 1e3,
+                             ncores = 20)
+    
+    rnet_combined = rnet_combined[,c("school_fastest_bicycle_sum","school_fastest_bicycle_go_dutch_sum","school_quietest_bicycle_sum",
+                                     "school_quietest_bicycle_go_dutch_sum","school_balanced_bicycle_sum","school_balanced_bicycle_go_dutch_sum",
+                                     "school_ebike_bicycle_sum","school_ebike_bicycle_go_dutch_sum","commute_fastest_bicycle_sum","commute_fastest_bicycle_go_dutch_sum",
+                                     "commute_balanced_bicycle_sum","commute_balanced_bicycle_go_dutch_sum","commute_quietest_bicycle_sum",
+                                     "commute_quietest_bicycle_go_dutch_sum","commute_ebike_bicycle_sum","commute_ebike_bicycle_go_dutch_sum",
+                                     "Gradient_max","Quietness_max","geometry")]
+    names(rnet_combined) = gsub("_sum","",names(rnet_combined))
+    names(rnet_combined) = gsub("_max","",names(rnet_combined))
+    
+    rnet_combined$all_fastest_bicycle = rnet_combined$school_fastest_bicycle + rnet_combined$commute_fastest_bicycle
+    rnet_combined$all_fastest_bicycle_go_dutch = rnet_combined$school_fastest_bicycle_go_dutch + rnet_combined$commute_fastest_bicycle_go_dutch
+    rnet_combined$all_quietest_bicycle = rnet_combined$school_quietest_bicycle + rnet_combined$commute_quietest_bicycle
+    rnet_combined$all_quietest_bicycle_go_dutch = rnet_combined$school_quietest_bicycle_go_dutch + rnet_combined$commute_quietest_bicycle_go_dutch
+    rnet_combined$all_balanced_bicycle = rnet_combined$school_balanced_bicycle + rnet_combined$commute_balanced_bicycle
+    rnet_combined$all_balanced_bicycle_go_dutch = rnet_combined$school_balanced_bicycle_go_dutch + rnet_combined$commute_balanced_bicycle_go_dutch
+    rnet_combined$all_ebike_bicycle = rnet_combined$school_ebike_bicycle + rnet_combined$school_ebike_bicycle
+    rnet_combined$all_ebike_bicycle_go_dutch  = rnet_combined$school_ebike_bicycle_go_dutch + rnet_combined$commute_ebike_bicycle_go_dutch
+    
     
     saveRDS(rnet, "outputdata/combined_network.Rds")
     rnet
@@ -337,7 +380,14 @@ list(
     file.rename("outputdata/combined_network.geojson", "rnet.geojson")
     # zip(zipfile = "outputdata/combined_network.zip", "rnet.geojson")
     # Tile the data:
-    system("bash code/tile.sh")
+    # system("bash code/tile.sh")
+    msg_verbose = paste0(
+      "--name=rnet --layer=rnet --attribution=UniverstyofLeeds --minimum-zoom=6 ",
+      "--maximum-zoom=13 --drop-smallest-as-needed --maximum-tile-bytes=5000000 ",
+      "--simplification=10 --buffer=5 --force  rnet.geojson"
+    )
+    msg = glue::glue("tippecanoe -o outputdata/rnet_{parameters$date_routing}.pmtiles")
+    system(paste(msg, msg_verbose))
     save_outputs
   }),
   
