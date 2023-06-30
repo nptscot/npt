c74c0cdb834d59bc16f8c4f696e43f65306a974b
Juan Fonseca Zamora <ts18jpf@leeds.ac.uk>
2023-06-30 13:43:01 +0100

    results of the test

--- code/tests/test-forth_bridge.R
+++ code/tests/test-forth_bridge.R
@@ -1,6 +1,31 @@
 library(sf)
-install.packages("mapedit")
+library(mapedit)
+library(tmap)
 
 desire_lines = read_rds("desire_lines_scotland.Rds")
 
 
+tmap_mode("view")
+
+
+tm_shape(desire_lines)+
+  tm_lines()
+
+
+line = mapedit::editMap()
+
+tm_shape(line)+
+  tm_lines()
+
+
+desire_lines_subset = desire_lines[line, ]
+
+sum(desire_lines_subset$bicycle)
+desire_lines_subset |>
+  filter(dist_euclidean < 2e4) |>
+  tm_shape() +
+  tm_lines(col = "bicycle")
+
+
+
+desire_lines_subset |> filter(dist_euclidean < 2e4) |> pull(bicycle) |> sum()
