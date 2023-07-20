a07c4cd08ed8872a529194706f489799faabb1ef
Malcolm Morgan <mem48@users.noreply.github.com>
2023-07-20 12:22:58 +0100

    Remove jitter - not needed

--- code/prep_commute_data.R
+++ code/prep_commute_data.R
@@ -55,6 +55,7 @@ unzip("../inputdata/SIMD/simd2020_withgeog.zip",
 zones <- read_sf(file.path(tempdir(),"SIMD/simd2020_withgeog/sc_dz_11.shp"))
 unlink(file.path(tempdir(),"SIMD"), recursive = TRUE)
 zones = zones[,"DataZone"]
+saveRDS(zones, "inputdata/DataZones.Rds")
 
 #OA Centroids
 dir.create(file.path(tempdir(),"OA"))
@@ -65,36 +66,6 @@ unlink(file.path(tempdir(),"OA"), recursive = TRUE)
 subpoints_origins = subpoints_origins[,"code"]
 subpoints_origins = st_transform(subpoints_origins, 4326)
 
-
-test = flow_wide[flow_wide$all > 100,]
-
-#Jitter
-flow_jitter = odjitter::jitter(
-  od = test,
-  zones = zones,
-  zones_d = zones,
-  origin_key = "DataZone",
-  destination_key = "DataZone",
-  subpoints_origins = subpoints_origins,
-  subpoints_destinations = subpoints_origins,
-  disaggregation_key = "all",
-  disaggregation_threshold = 30,
-  show_command = TRUE
-)
-
-# command sent to the system:                                                                                               
-# odjitter jitter --od-csv-path C:\Users\earmmor\AppData\Local\Temp\RtmpmasHPh/od.csv --zones-path C:\Users\earmmor\AppData\Local\Temp\RtmpmasHPh/zones.geojson --zone-name-key DataZone --origin-key DataZone --destination-key DataZone --subpoints-origins-path C:\Users\earmmor\AppData\Local\Temp\RtmpmasHPh/subpoints_origins.geojson --subpoints-destinations-path C:\Users\earmmor\AppData\Local\Temp\RtmpmasHPh/subpoints_destinations.geojson --disaggregation-key all --disaggregation-threshold 30 --rng-seed 38736 --deduplicate-pairs  --output-path C:\Users\earmmor\AppData\Local\Temp\RtmpmasHPh/od_jittered.geojsonScraped 6976 zones from C:\Users\earmmor\AppData\Local\Temp\RtmpmasHPh/zones.geojson
-# Scraped 46351 subpoints from C:\Users\earmmor\AppData\Local\Temp\RtmpmasHPh/subpoints_origins.geojson
-# Scraped 46351 subpoints from C:\Users\earmmor\AppData\Local\Temp\RtmpmasHPh/subpoints_destinations.geojson
-# Disaggregating OD data
-# thread 'main' panicked at 'no entry found for key', C:\Users\earmmor\.cargo\git\checkouts\odjitter-2ee039e64a6d7c8d\af35f08\src\lib.rs:203:55
-# note: run with `RUST_BACKTRACE=1` environment variable to display a backtrace
-# Error: Cannot open "C:\Users\earmmor\AppData\Local\Temp\RtmpmasHPh\od_jittered.geojson"; The source could be corrupt or not supported. See `st_drivers()` for a list of supported formats.
-# In addition: Warning messages:
-#   1: In CPL_read_ogr(dsn, layer, query, as.character(options), quiet,  :
-#                        GDAL Error 1: At line 2, character 1: Unterminated array
-#                      2: In CPL_read_ogr(dsn, layer, query, as.character(options), quiet,  :
-#                                           GDAL Error 4: Failed to read GeoJSON data
 
 dl = od2line(flow_wide, datazone_cent)
 
