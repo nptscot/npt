dbb5910836c9485c1cc26522e0a17e67c64c04f2
robinlovelace <rob00x@gmail.com>
2023-07-20 14:29:57 +0100

    Tweak devcontainer, install Rust non-interactively

--- .devcontainer/install-additional-dependencies.sh
+++ .devcontainer/install-additional-dependencies.sh
@@ -1,7 +1,7 @@
 # Aim: install Rust, cargo and then then make the https://github.com/dabreegster/odjitter crate available from the command line:
 
 # Install Rust:
-curl https://sh.rustup.rs -sSf | sh
+curl https://sh.rustup.rs -sSf | sh -s -- -y
 
 # Add cargo to the path:
 export PATH="$HOME/.cargo/bin:$PATH"
