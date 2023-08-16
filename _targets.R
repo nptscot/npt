907f91532065d1ad037235d57fc292516a3d56b5
Atum Scott <atumscot@gmail.com>
2023-08-16 13:53:15 +0100

    fix list bug

--- _targets.R
+++ _targets.R
@@ -248,7 +248,7 @@ list(
     uptake_list_school = lapply(parameters$plan, function(p) {
       message("Uptake for ", p, " school routes")
       names(r_school[[1]])
-      routes = r_school[[p]] %>%
+      routes = r_school[[p]]$routes %>%
         mutate(all = count) %>% 
         get_scenario_go_dutch(purpose = "school") %>%
         as_tibble()
