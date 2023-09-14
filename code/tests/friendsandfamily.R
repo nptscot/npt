2f585451b92de1b468276407b8331d386594f309
Joey Talbot <joeytalbot85@gmail.com>
2023-09-14 10:58:14 +0100

    reorder

--- code/tests/friendsandfamily.R
+++ code/tests/friendsandfamily.R
@@ -4,6 +4,7 @@ devtools::install_github("robinlovelace/ukboundaries")
 library(ukboundaries)
 devtools::install_github("robinlovelace/simodels")
 library(simodels)
+library(tmap)
 source("R/gravity_model.R")
 
 disag_threshold = 1000 # increasing this reduces the number of od pairs
@@ -12,32 +13,39 @@ min_distance_meters = 500 # does this mean that any shops closer than 500m away 
 # It would be better to route to these, then exclude them afterwards as too close for the trip to be worth cycling
 max_length_euclidean_km = 5
 
+zones = readRDS("inputdata/DataZones.Rds")
+
 osm_highways = readRDS("./inputdata/osm_highways_2023-08-09.Rds")
 
-scot_zones = st_read("./data-raw/Scottish_Parliamentary_Constituencies_December_2022_Boundaries_SC_BGC_-9179620948196964406.gpkg")
-grid = st_make_grid(scot_zones, cellsize = 500, what = "centers")
-grid_df = data.frame(grid)
-grid_df = tibble::rowid_to_column(grid_df, "grid_id")
-
-highways_bng = osm_highways %>% 
-  st_transform(27700)
-highways = highways_bng %>% 
-  mutate(grid_id = st_nearest_feature(highways_bng, grid))
-
-# calculate weighting of each grid point
-highways_grid = highways %>% 
-  st_drop_geometry() %>% 
-  group_by(grid_id) %>% 
-  summarise(size = n())
-
-# assign grid geometry
-highways_join = inner_join(grid_df, highways_grid)
-highways_sf = st_as_sf(highways_join)
-highways_sf = st_transform(highways_sf, 4326)
-# tm_shape(highways_sf) + tm_dots("size") # check points look right
-
-saveRDS(highways_sf, "./inputdata/highways_grid.Rds")
+# This section only needs to run once
+# scot_zones = st_read("./data-raw/Scottish_Parliamentary_Constituencies_December_2022_Boundaries_SC_BGC_-9179620948196964406.gpkg")
+# grid = st_make_grid(scot_zones, cellsize = 100, what = "centers")
+# grid_df = data.frame(grid)
+# grid_df = tibble::rowid_to_column(grid_df, "grid_id")
+# 
+# highways_bng = osm_highways %>% 
+#   st_transform(27700)
+# highways = highways_bng %>% 
+#   mutate(grid_id = st_nearest_feature(highways_bng, grid))
+# 
+# # calculate weighting of each grid point
+# highways_grid = highways %>% 
+#   st_drop_geometry() %>% 
+#   group_by(grid_id) %>% 
+#   summarise(size = n())
+# 
+# # assign grid geometry
+# highways_join = inner_join(grid_df, highways_grid)
+# highways_sf = st_as_sf(highways_join)
+# highways_sf = st_transform(highways_sf, 4326)
+#
+# check points look right
+# tm_shape(zones) + tm_polygons() +
+# tm_shape(highways_sf) + tm_dots("size")
+# 
+# saveRDS(highways_sf, "./inputdata/highways_grid.Rds")
 highways_grid = readRDS("./inputdata//highways_grid.Rds")
+
 
 
 trip_purposes = read.csv("./data-raw/scottish-household-survey-2012-19.csv")
@@ -51,7 +59,6 @@ visiting_percent = trip_purposes %>%
   select(adjusted_mean)
 visiting_percent = visiting_percent[[1]]/100
 
-zones = readRDS("inputdata/DataZones.Rds")
 zones_visiting = zones %>%
   mutate(visiting_trips = ResPop2011 * 2.61 * visiting_percent) # resident population (should use 18+ only) * trips per person (from NTS 2019 England) * percent of trips that are for visiting
 
