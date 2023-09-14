af199ef744fb2b601c2ecba94420c8e4f6fc18cf
Joey Talbot <joeytalbot85@gmail.com>
2023-09-14 12:07:24 +0100

    intermediate zones

--- code/tests/friendsandfamily.R
+++ code/tests/friendsandfamily.R
@@ -5,6 +5,7 @@ library(ukboundaries)
 devtools::install_github("robinlovelace/simodels")
 library(simodels)
 library(tmap)
+tmap_mode("view")
 source("R/gravity_model.R")
 
 disag_threshold = 1000 # increasing this reduces the number of od pairs
@@ -13,40 +14,59 @@ min_distance_meters = 500 # does this mean that any shops closer than 500m away 
 # It would be better to route to these, then exclude them afterwards as too close for the trip to be worth cycling
 max_length_euclidean_km = 5
 
+
+# Geographic zones --------------------------------------------------------
+
+# Data Zones have 500-1000 residents
+# https://spatialdata.gov.scot/geonetwork/srv/api/records/7d3e8709-98fa-4d71-867c-d5c8293823f2
+# Intermediate Zones have 2500-6000 residents, there are 1279 in Scotland in 2011
+# https://www.data.gov.uk/dataset/133d4983-c57d-4ded-bc59-390c962ea280/intermediate-zone-boundaries-2011#:~:text=Intermediate%20zones%20also%20represent%20a,covering%20the%20whole%20of%20Scotland.
+
+# Population stats are also available from https://statistics.gov.scot/slice?dataset=http%3A%2F%2Fstatistics.gov.scot%2Fdata%2Fpopulation-estimates-2011-datazone-linked-dataset&http%3A%2F%2Fpurl.org%2Flinked-data%2Fcube%23measureType=http%3A%2F%2Fstatistics.gov.scot%2Fdef%2Fmeasure-properties%2Fcount&http%3A%2F%2Fstatistics.gov.scot%2Fdef%2Fdimension%2Fage=http%3A%2F%2Fstatistics.gov.scot%2Fdef%2Fconcept%2Fage%2Fall&http%3A%2F%2Fstatistics.gov.scot%2Fdef%2Fdimension%2Fsex=http%3A%2F%2Fstatistics.gov.scot%2Fdef%2Fconcept%2Fsex%2Fall
+
+# DataZones (too small - makes for slow jittering)
 zones = readRDS("inputdata/DataZones.Rds")
+
+# Intermediate Zones
+izones = read_sf("./inputdata/SG_IntermediateZoneBdry_2011/SG_IntermediateZone_Bdry_2011.shp")
+
+
+# Highways data -----------------------------------------------------------
 
 osm_highways = readRDS("./inputdata/osm_highways_2023-08-09.Rds")
 
 # This section only needs to run once
 # scot_zones = st_read("./data-raw/Scottish_Parliamentary_Constituencies_December_2022_Boundaries_SC_BGC_-9179620948196964406.gpkg")
-# grid = st_make_grid(scot_zones, cellsize = 100, what = "centers")
+# grid = st_make_grid(scot_zones, cellsize = 500, what = "centers")
 # grid_df = data.frame(grid)
 # grid_df = tibble::rowid_to_column(grid_df, "grid_id")
 # 
-# highways_bng = osm_highways %>% 
+# highways_bng = osm_highways %>%
 #   st_transform(27700)
-# highways = highways_bng %>% 
+# highways = highways_bng %>%
 #   mutate(grid_id = st_nearest_feature(highways_bng, grid))
 # 
 # # calculate weighting of each grid point
-# highways_grid = highways %>% 
-#   st_drop_geometry() %>% 
-#   group_by(grid_id) %>% 
+# highways_grid = highways %>%
+#   st_drop_geometry() %>%
+#   group_by(grid_id) %>%
 #   summarise(size = n())
 # 
 # # assign grid geometry
 # highways_join = inner_join(grid_df, highways_grid)
 # highways_sf = st_as_sf(highways_join)
 # highways_sf = st_transform(highways_sf, 4326)
-#
-# check points look right
-# tm_shape(zones) + tm_polygons() +
-# tm_shape(highways_sf) + tm_dots("size")
 # 
 # saveRDS(highways_sf, "./inputdata/highways_grid.Rds")
-highways_grid = readRDS("./inputdata//highways_grid.Rds")
+
+highways_grid = readRDS("./inputdata/highways_grid.Rds")
+
+# check points look right
+# tm_shape(izones) + tm_polygons() +
+#   tm_shape(highways_grid) + tm_dots("size")
 
 
+# Trip purposes -----------------------------------------------------------
 
 trip_purposes = read.csv("./data-raw/scottish-household-survey-2012-19.csv")
 go_home = trip_purposes$Mean[trip_purposes$Purpose == "Go Home"]
@@ -59,8 +79,11 @@ visiting_percent = trip_purposes %>%
   select(adjusted_mean)
 visiting_percent = visiting_percent[[1]]/100
 
-zones_visiting = zones %>%
+zones_visiting = izones %>%
   mutate(visiting_trips = ResPop2011 * 2.61 * visiting_percent) # resident population (should use 18+ only) * trips per person (from NTS 2019 England) * percent of trips that are for visiting
+
+
+# Spatial interaction model -----------------------------------------------
 
 # Spatial interaction model of journeys
 od_visiting = si_to_od(zones_visiting, zones_visiting, max_dist = max_length_euclidean_km * 1000)
