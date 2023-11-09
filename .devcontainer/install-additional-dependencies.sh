8ec827b84fa046abd009fb2cc4c8c8d23ecf03e5
robinlovelace <rob00x@gmail.com>
2023-11-09 11:47:56 +0000

    Update install deps sh file

--- .devcontainer/install-additional-dependencies.sh
+++ .devcontainer/install-additional-dependencies.sh
@@ -1,9 +1,20 @@
 # Aim: install dependencies
 
+# Check the Linux distro we're running:
+cat /etc/os-release
+
 # Source the R script code/install.R from command line:
 Rscript code/install.R
 
+# Install Python + pip in Debian:
+# Add repository:
+echo "deb http://archive.ubuntu.com/ubuntu/ jammy main restricted" >> /etc/apt/sources.list
+apt-get update
+apt-get install -y software-properties-common
+apt-get install -y python3 python3-pip
+
 # Install Python dependencies:
+pip install pandas
 pip install -r requirements.txt
 
 # Rust, cargo and then then make the https://github.com/dabreegster/odjitter crate available from the command line:
--- _targets.R
+++ _targets.R
@@ -7,28 +7,8 @@
 # If your RAM limited use tar_make() to run one job at a time
 
 # Load packages required to define the pipeline:
-if(FALSE){ # Repeated builds can it GitHub API limit, set to TRUE to check for package updates
-  remotes::install_dev("cyclestreets")
-  remotes::install_github("dabreegster/odjitter", subdir = "r")
-  remotes::install_github("ropensci/stplanr")# Improved overline
-}
+source("code/install.R")
 
-library(targets)
-library(magrittr) # Light load of %>%
-library(sf)
-library(future) # Needed for multi-core running
-library(future.callr)
-library(stplanr)
-library(dplyr)
-# Set target options:
-pkgs = packages = c(
-  "tibble","zonebuilder","dplyr","lubridate",
-  "stringr","sf","tidyr","data.table", "targets",
-  "glue","zip","jsonlite","remotes","gert","collapse","pct",
-  "readr", "future", "future.callr", "future.batchtools",
-  "bs4Dash", "DT", "gt", "pingr", "shinybusy", "shinyWidgets"
-)
-remotes::install_cran(pkgs)
 tar_option_set(
   memory = "transient", 
   garbage_collection = TRUE,
