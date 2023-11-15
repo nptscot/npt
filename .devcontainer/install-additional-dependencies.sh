7b7f2f0733dcd458c5c59bf344ba248dee78510b
atumscott <119737782+atumscott@users.noreply.github.com>
2023-11-15 21:24:05 +0000

    317 update devcontainer for reproducible build (#322)
    * Update with more dependencies (#317)
    
    * Update install deps sh file
    
    * add requirements.txt and modify Dockerfile
    
    * Remove unnused files
    
    * Separate install from loading pkgs
    
    * Change target checks, close #318
    
    * Update with working target order (#318)
    
    * Install tippecanoe
    
    * add changes (#321)
    
    * Update odjitter location
    
    * save changes
    
    * Update install script
    
    * Add path to profile not ~/.bashrc
    
    * Update with sys path update
    
    * Fix typo
    
    * Add is_bin_on_path() function
    
    * One more fix to get it working, full build!
    
    * tidy R code
    
    * remove unnecessary lines
    
    * remove odjitter_location and tar_target(rnet_simple), add file download for rnet_x
    
    * remove required line 153
    
    * add back odjitter_location
    
    * Add geos method for fast buffering
    
    * add geos into install.R
    
    * update buffer methods
    
    * Add simd check
    
    * Update odjitter location depending on path
    
    * callr
    
    * Update so build passes without zones_contextual
    
    ---------
    
    Co-authored-by: robinlovelace <rob00x@gmail.com>
    Co-authored-by: wangzhao0217 <wangzhao0217@gmail.com>
    Co-authored-by: wangzhao0217 <74598734+wangzhao0217@users.noreply.github.com>
    Co-authored-by: Robin Lovelace <Robinlovelace@users.noreply.github.com>

--- .devcontainer/install-additional-dependencies.sh
+++ .devcontainer/install-additional-dependencies.sh
@@ -1,4 +1,25 @@
-# Aim: install Rust, cargo and then then make the https://github.com/dabreegster/odjitter crate available from the command line:
+# Aim: install dependencies
+
+# Check the Linux distro we're running:
+cat /etc/os-release
+
+# # Add atumscott to docker group:
+# usermod -aG docker atumscott
+
+# Source the R script code/install.R from command line:
+Rscript code/install.R
+
+# Install Python + pip in Debian:
+# Add repository:
+echo "deb http://archive.ubuntu.com/ubuntu/ jammy main restricted" >> /etc/apt/sources.list
+apt-get update
+apt-get install -y software-properties-common
+apt-get install -y python3 python3-pip
+
+# Install Python dependencies:
+pip install -r requirements.txt
+
+# Rust, cargo and then then make the https://github.com/dabreegster/odjitter crate available from the command line:
 
 # Install Rust:
 curl https://sh.rustup.rs -sSf | sh -s -- -y
@@ -6,11 +27,16 @@ curl https://sh.rustup.rs -sSf | sh -s -- -y
 # Add cargo to the path:
 export PATH="$HOME/.cargo/bin:$PATH"
 
-# Add cargo to the path permanently:
-echo 'export PATH="$HOME/.cargo/bin:$PATH"' >> ~/.bashrc
+# Add cargo to the path permanently at ~/.profile:
+echo 'export PATH="$HOME/.cargo/bin:$PATH"' >> ~/.profile
 
 # Install odjitter:
-cargo install --git https://github.com/dabreegster/odjitter odjitter
+cargo install --git https://github.com/dabreegster/odjitter --rev 32fb58bf7f0d68afd3b76b88cf6b1272c5c66828
 
-# Install targets
-Rscript -e 'install.packages("targets")'
+# Install tippecannoe
+cd /tmp
+git clone https://github.com/felt/tippecanoe.git
+cd tippecanoe
+make -j
+sudo make install
+tippecanoe --version
---
+++ code/install.R
@@ -0,0 +1,15 @@
+# Set target options:
+pkgs = packages = c(
+  "tibble","zonebuilder","dplyr","lubridate",
+  "stringr","sf","tidyr","data.table", "targets",
+  "glue","zip","jsonlite","remotes","gert","collapse","pct",
+  "readr", "future", "future.callr", "future.batchtools",
+  "bs4Dash", "DT", "gt", "pingr", "shinybusy", "shinyWidgets","geos"
+)
+remotes::install_cran(pkgs)
+
+if(TRUE){ # Repeated builds can it GitHub API limit, set to TRUE to check for package updates
+  remotes::install_dev("cyclestreets")
+  remotes::install_github("dabreegster/odjitter", subdir = "r")
+  remotes::install_github("ropensci/stplanr")# Improved overline
+}
