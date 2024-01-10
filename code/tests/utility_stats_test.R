79681ffeb16e3c3984a8a1dc3d1e9593ebabefbc
Atum Scott <atumscot@gmail.com>
2024-01-10 13:00:25 +0000

    destinations

--- code/tests/utility_stats_test.R
+++ code/tests/utility_stats_test.R
@@ -58,6 +58,39 @@ shopping_orig_ebike = shopping_ebike %>%
 sum(shopping_orig_ebike, na.rm = T)
 # [1] 14786.86
 
+# Test shopping destinations
+
+shopping_dest_base = shopping_base %>% 
+  ungroup %>% 
+  select(grep("dest", names(shopping_base)) &
+           !grep("route", names(shopping_base)) &
+           !grep("quiet", names(shopping_base)) &
+           !grep("all", names(shopping_base)))
+sum(shopping_dest_base, na.rm = T)
+# [1] 9607.076
+
+sum(utility_stats$shopping_dest_all, na.rm = T)
+# [1] 9607.076
+
+shopping_dest_dutch = shopping_dutch %>% 
+  ungroup %>% 
+  select(grep("dest", names(shopping_dutch)) &
+           !grep("route", names(shopping_dutch)) &
+           !grep("quiet", names(shopping_dutch)) &
+           !grep("all", names(shopping_dutch)))
+sum(shopping_dest_dutch, na.rm = T)
+# [1] 17291.57
+
+shopping_dest_ebike = shopping_ebike %>% 
+  ungroup %>% 
+  select(grep("dest", names(shopping_ebike)) &
+           !grep("route", names(shopping_ebike)) &
+           !grep("quiet", names(shopping_ebike)) &
+           !grep("all", names(shopping_ebike)))
+sum(shopping_dest_ebike, na.rm = T)
+# [1] 14786.86
+
+
 
 # Test consistency between scenarios - visiting
 # these are consistent
@@ -102,3 +135,37 @@ visiting_orig_ebike = visiting_ebike %>%
 sum(visiting_orig_ebike, na.rm = T)
 # [1] 5405.999
 
+
+# Test visiting destinations
+
+visiting_dest_base = visiting_base %>% 
+  ungroup %>% 
+  select(grep("dest", names(visiting_base)) &
+           !grep("route", names(visiting_base)) &
+           !grep("quiet", names(visiting_base)) &
+           !grep("all", names(visiting_base)))
+sum(visiting_dest_base, na.rm = T)
+# [1] 5405.999
+
+sum(utility_stats$visiting_dest_all, na.rm = T)
+# [1] 5405.999
+
+
+visiting_dest_dutch = visiting_dutch %>% 
+  ungroup %>% 
+  select(grep("dest", names(visiting_dutch)) &
+           !grep("route", names(visiting_dutch)) &
+           !grep("quiet", names(visiting_dutch)) &
+           !grep("all", names(visiting_dutch)))
+sum(visiting_dest_dutch, na.rm = T)
+# [1] 5405.999
+
+
+visiting_dest_ebike = visiting_ebike %>% 
+  ungroup %>% 
+  select(grep("dest", names(visiting_ebike)) &
+           !grep("route", names(visiting_ebike)) &
+           !grep("quiet", names(visiting_ebike)) &
+           !grep("all", names(visiting_ebike)))
+sum(visiting_dest_ebike, na.rm = T)
+# [1] 5405.999
