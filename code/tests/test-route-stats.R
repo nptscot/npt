8fe3959c0342e935ce5ee724c44fc003adc26395
joeytalbot <52210872+joeytalbot@users.noreply.github.com>
2024-01-17 12:20:16 +0000

    quietest routes

--- code/tests/test-route-stats.R
+++ code/tests/test-route-stats.R
@@ -59,6 +59,60 @@ sum(shopping_orig_ebike, na.rm = T)
 # [1] 19312.9
 
 
+# Quietest routes-------------------------------------
+
+tar_load(uptake_utility_quietest)
+
+routes = uptake_utility_quietest
+
+dutch = routes %>% 
+  select(grep("dutch", names(routes)), purpose)
+
+ebike = routes %>% 
+  select(grep("ebike", names(routes)), purpose)
+
+base = routes %>% 
+  select(!grep("dutch", names(routes)) & !grep("ebike", names(routes)))
+
+
+# Test consistency between scenarios: shopping ----------------------------
+# these are not consistent
+
+shopping_base = base %>% 
+  filter(purpose == "shopping")
+
+shopping_orig_base = shopping_base %>% 
+  ungroup() %>% 
+  sf::st_drop_geometry() %>% 
+  select(bicycle, car, foot, taxi, public_transport)
+sum(shopping_orig_base, na.rm = T)
+# [1] 19312.9
+
+routes_shopping = routes %>% 
+  filter(purpose == "shopping")
+sum(routes_shopping$all, na.rm = T)
+# [1] 19312.9
+
+shopping_dutch = dutch %>% 
+  filter(purpose == "shopping")
+
+shopping_orig_dutch = shopping_dutch %>% 
+  sf::st_drop_geometry() %>% 
+  select(bicycle_go_dutch, car_go_dutch, foot_go_dutch, taxi_go_dutch, public_transport_go_dutch)
+sum(shopping_orig_dutch, na.rm = T)
+# [1] 19312.9
+
+shopping_ebike = ebike %>% 
+  filter(purpose == "shopping")
+
+shopping_orig_ebike = shopping_ebike %>% 
+  sf::st_drop_geometry() %>% 
+  select(bicycle_ebike, car_ebike, foot_ebike, taxi_ebike, public_transport_ebike)
+sum(shopping_orig_ebike, na.rm = T)
+# [1] 19312.9
+
+
+
 
 # Test consistency between scenarios: visiting ----------------------------
 # these are consistent
@@ -96,3 +150,4 @@ visiting_orig_ebike = visiting_ebike %>%
   select(bicycle_ebike, car_ebike, foot_ebike, taxi_ebike, public_transport_ebike)
 sum(visiting_orig_ebike, na.rm = T)
 # [1] 5307.008
+
