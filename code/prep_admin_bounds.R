edf94b706c7512869db392a05592a978d452daea
robinlovelace <rob00x@gmail.com>
2024-02-16 06:52:19 +0000

    Update assignment operator

--- code/prep_admin_bounds.R
+++ code/prep_admin_bounds.R
@@ -18,15 +18,15 @@ library(sf)
 # la = sf::read_sf("la.gpkg")
 # plot(la)
 
-la <- sf::read_sf("https://services1.arcgis.com/ESMARspQHYMw9BZ9/arcgis/rest/services/Local_Authority_Districts_December_2023_Boundaries_UK_BSC/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson")
+la = sf::read_sf("https://services1.arcgis.com/ESMARspQHYMw9BZ9/arcgis/rest/services/Local_Authority_Districts_December_2023_Boundaries_UK_BSC/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson")
 
-la <- la[, c("LAD23CD", "LAD23NM")]
+la = la[, c("LAD23CD", "LAD23NM")]
 
 
 
 
 # LAs in Scotland, CD starts with "S":
-la_scotland <- la[grepl("^S", la$LAD23CD),]
+la_scotland = la[grepl("^S", la$LAD23CD),]
 
 # https://www.publiccontractsscotland.gov.uk/Maps/LocalAuthorityRegions.aspx
 
@@ -71,7 +71,7 @@ la_to_region_lookup = tibble::tribble(
     "Scotland South", "Dumfries and Galloway",
     "Scotland South", "Scottish Borders"
 )
-la_regions <- dplyr::left_join(la_scotland, la_to_region_lookup)
+la_regions = dplyr::left_join(la_scotland, la_to_region_lookup)
 la_regions |>
   select(Region) |>
   plot()
@@ -91,9 +91,9 @@ path_westminster = "Westminster_Parliamentary_Constituencies_(Dec_2021)_UK_BFC.z
 dir.create(file.path(tempdir(),"zones"))
 unzip(file.path(path, path_westminster), exdir = file.path(tempdir(),"zones"))
 
-westminster <- read_sf(file.path(tempdir(),"zones","Westminster_Parliamentary_Constituencies_(Dec_2021)_UK_BFC.shp"))
-westminster <- westminster[,c("PCON21CD")]
-westminster <- st_transform(westminster, 4326)
+westminster = read_sf(file.path(tempdir(),"zones","Westminster_Parliamentary_Constituencies_(Dec_2021)_UK_BFC.shp"))
+westminster = westminster[,c("PCON21CD")]
+westminster = st_transform(westminster, 4326)
 st_write(westminster,"../atumscot/outputs/westminster.geojson")
 unlink(file.path(tempdir(),"zones"), recursive = TRUE)
 
@@ -102,9 +102,9 @@ path_holyrood = "Scottish_Parliamentary_Constituencies_(May_2021)_Boundaries_SC_
 dir.create(file.path(tempdir(),"zones"))
 unzip(file.path(path, path_holyrood), exdir = file.path(tempdir(),"zones"))
 
-holyrood <- read_sf(file.path(tempdir(),"zones","SPC_MAY_2021_SC_BFC.shp"))
-holyrood <- holyrood[,c("SPC21CD")]
-holyrood <- st_transform(holyrood, 4326)
+holyrood = read_sf(file.path(tempdir(),"zones","SPC_MAY_2021_SC_BFC.shp"))
+holyrood = holyrood[,c("SPC21CD")]
+holyrood = st_transform(holyrood, 4326)
 st_write(holyrood,"../atumscot/outputs/holyrood.geojson")
 unlink(file.path(tempdir(),"zones"), recursive = TRUE)
 
@@ -113,16 +113,16 @@ path_wards = "Wards_(December_2022)_Boundaries_GB_BFC.zip"
 dir.create(file.path(tempdir(),"zones"))
 unzip(file.path(path, path_wards), exdir = file.path(tempdir(),"zones"))
 
-wards <- read_sf(file.path(tempdir(),"zones","WD_DEC_22_GB_BFC.shp"))
-wards <- wards[,c("WD22CD")]
-wards <- st_transform(wards, 4326)
+wards = read_sf(file.path(tempdir(),"zones","WD_DEC_22_GB_BFC.shp"))
+wards = wards[,c("WD22CD")]
+wards = st_transform(wards, 4326)
 st_write(wards,"../atumscot/outputs/wards.geojson")
 unlink(file.path(tempdir(),"zones"), recursive = TRUE)
 
 # Regional
 path_regional = "D:/OneDrive - University of Leeds/Data/OS/Boundary Line/bdline_gpkg_gb/Data/bdline_gb.gpkg"
-regions <- read_sf(path_regional, "scotland_and_wales_region")
-regions <- regions[regions$Area_Description == "Scottish Parliament Electoral Region",]
-regions <- regions[,c("Census_Code")]
-regions <- st_transform(regions, 4326)
+regions = read_sf(path_regional, "scotland_and_wales_region")
+regions = regions[regions$Area_Description == "Scottish Parliament Electoral Region",]
+regions = regions[,c("Census_Code")]
+regions = st_transform(regions, 4326)
 st_write(regions,"../atumscot/outputs/scot_regions.geojson")
