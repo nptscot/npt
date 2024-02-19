561062b22cab4436f75b9476cc7debeb7af3fae5
Atum Scott <atumscot@gmail.com>
2024-02-19 16:12:11 +0000

    Try for another region

--- code/build.R
+++ code/build.R
@@ -4,11 +4,14 @@ parameters = jsonlite::read_json("parameters.json", simplifyVector = T)
 
 lads = sf::read_sf("inputdata/boundaries/la_regions_2023.geojson")
 region_names = unique(lads$Region)
+region_names_lowercase = snakecase::to_snake_case(region_names)
 
 for (region in region_names) {
   parameters$region = region
   jsonlite::write_json(parameters, "parameters.json")
   targets::tar_make()
 }
-```
+
+# Combine the outputs:
+
   
