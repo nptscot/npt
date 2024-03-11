132268a9aaddd445f0f7e47e358e910257e805c6
wangzhao0217 <wangzhao0217@gmail.com>
2024-03-11 17:49:21 +0000

    add R/coherent_network_group.R to streamline the coherent

---
+++ R/coherent_network_group.R
@@ -0,0 +1,24 @@
+coherent_network_group = function(CITY, ZONE, min_percentile, arterial = FALSE) {
+  # Generate coherent network
+  rnet_coherent = cohesive_network(network_tile = CITY, combined_grid_buffer = ZONE, arterial = arterial, min_percentile = min_percentile)
+  
+  # Select relevant columns
+  rnet_coherent_selected = rnet_coherent |>
+    dplyr::select(all_fastest_bicycle_go_dutch, weight)
+  
+  # Group and process the network
+  grouped_net = rnet_coherent_selected |>
+    sfnetworks::as_sfnetwork(directed = FALSE) |>
+    tidygraph::morph(to_linegraph) |>
+    dplyr::mutate(group = tidygraph::group_edge_betweenness(n_groups = 12)) |>
+    tidygraph::unmorph() |>
+    tidygraph::activate(edges) |>
+    sf::st_as_sf() |>
+    sf::st_transform("EPSG:4326") |>
+    dplyr::group_by(group) |>
+    dplyr::summarise(mean_potential = mean(weight, na.rm = TRUE)) |>
+    dplyr::mutate(group = rank(-mean_potential))
+  
+  # Return the processed network
+  grouped_net
+}
