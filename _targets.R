ab47f030515d346288a784610280974fb0fd85c7
robinlovelace <rob00x@gmail.com>
2024-04-26 22:56:48 +0100

    Tidy up file paths, close https://github.com/nptscot/internal/issues/36

--- _targets.R
+++ _targets.R
@@ -1069,14 +1069,14 @@ tar_target(zones_contextual, {
     message("SIMD data not found, skipping this target")
     return(NULL)
   }
-  dir.create(file.path(tempdir(),"SIMD"))
-  unzip(f_simd, exdir = file.path(tempdir(),"SIMD"))
-  files = list.files(file.path(tempdir(),"SIMD/simd2020_withgeog"), full.names = TRUE)
+  dir.create(file.path(parameters$local_dir,"SIMD"))
+  unzip(f_simd, exdir = file.path(parameters$local_dir,"SIMD"))
+  files = list.files(file.path(parameters$local_dir,"SIMD/simd2020_withgeog"), full.names = TRUE)
   
-  zones = sf::read_sf(file.path(tempdir(),"SIMD/simd2020_withgeog/sc_dz_11.shp"))
-  simd = readr::read_csv(file.path(tempdir(),"SIMD/simd2020_withgeog/simd2020_withinds.csv"))
+  zones = sf::read_sf(file.path(parameters$local_dir,"SIMD/simd2020_withgeog/sc_dz_11.shp"))
+  simd = readr::read_csv(file.path(parameters$local_dir,"SIMD/simd2020_withgeog/simd2020_withinds.csv"))
   
-  unlink(file.path(tempdir(),"SIMD"), recursive = TRUE)
+  unlink(file.path(parameters$local_dir,"SIMD"), recursive = TRUE)
   
   zones = zones[,c("DataZone","Name","TotPop2011","ResPop2011","HHCnt2011")]
   simd$Intermediate_Zone = NULL
