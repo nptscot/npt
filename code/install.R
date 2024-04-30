ba2769d8cf169f8c63155996ce1952b78e438fa0
wangzhao0217 <wangzhao0217@gmail.com>
2024-04-30 18:22:39 +0100

    add code to install.R

--- code/install.R
+++ code/install.R
@@ -1,3 +1,16 @@
+pkgs  = c(
+  "tibble","zonebuilder","dplyr","lubridate",
+  "stringr","sf","tidyr","data.table", "targets",
+  "glue","zip","jsonlite","remotes","gert","collapse","pct",
+  "readr",
+  "bs4Dash", "DT", "gt", "pingr", "shinybusy", "shinyWidgets","geos",
+  "cyclestreets" ,"stplanr", "simodels",
+  "geojsonsf","lwgeom","targets","tidyverse", "crew"
+)
+
+#Do you want to reinstall github packages, set to TRUE for first run
+update_github_packages = TRUE
+
 remotes::install_cran(pkgs)
 
 # Repeated builds can it GitHub API limit, set to TRUE in _targets.R to check for package updates
