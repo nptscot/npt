90d4ee77ec33a6f1ce66f66f3a4cf0a1d09ba4d9
robinlovelace <rob00x@gmail.com>
2024-05-15 21:42:02 +0100

    styler.equals::style_file("code/install.R")

--- code/install.R
+++ code/install.R
@@ -1,31 +1,31 @@
 pkgs = c(
-  "crew",         # For targets with workers
+  "crew", # For targets with workers
   "cyclestreets", # For routing
-  "geojsonsf",    # For converting geojson to sf
-  "geos",         # For geometric operations
-  "gert",         # For interactive with git
-  "glue",         # For string interpolation
-  "lubridate",    # For working with dates and times
-  "lwgeom",       # For working with spatial data
-  "osmextract",   # For extracting OpenStreetMap data
-  "pct",          # PCT interface
-  "remotes",      # For installing packages from remote sources
-  "sf",           # For working with spatial data
-  "simodels",     # For spatial interaction models
-  "snakecase",    # For converting strings to snake case
-  "stplanr",      # For sustainable transport planning
-  "targets",      # For managing targets in a workflow
-  "tidyverse",    # Includes dplyr, ggplot2, tidyr, stringr etc.
-  "zonebuilder"   # For creating zones for spatial analysis
+  "geojsonsf", # For converting geojson to sf
+  "geos", # For geometric operations
+  "gert", # For interactive with git
+  "glue", # For string interpolation
+  "lubridate", # For working with dates and times
+  "lwgeom", # For working with spatial data
+  "osmextract", # For extracting OpenStreetMap data
+  "pct", # PCT interface
+  "remotes", # For installing packages from remote sources
+  "sf", # For working with spatial data
+  "simodels", # For spatial interaction models
+  "snakecase", # For converting strings to snake case
+  "stplanr", # For sustainable transport planning
+  "targets", # For managing targets in a workflow
+  "tidyverse", # Includes dplyr, ggplot2, tidyr, stringr etc.
+  "zonebuilder" # For creating zones for spatial analysis
 )
 
-#Do you want to reinstall github packages, set to TRUE for first run
+# Do you want to reinstall github packages, set to TRUE for first run
 update_github_packages = TRUE
 options(Ncpus = 4)
 remotes::install_cran(pkgs)
 
 # Repeated builds can it GitHub API limit, set to TRUE in _targets.R to check for package updates
-if(update_github_packages){ 
+if (update_github_packages) {
   remotes::install_dev("cyclestreets")
   remotes::install_github("dabreegster/odjitter", subdir = "r")
   remotes::install_github("robinlovelace/ukboundaries")
