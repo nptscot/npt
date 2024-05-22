2b57ef84b16b00021baace9a354074bfe0c0072b
robinlovelace <rob00x@gmail.com>
2024-05-22 22:38:43 +0100

    Update run.sh

--- R/get_routes.R
+++ R/get_routes.R
@@ -14,8 +14,8 @@ get_routes = function(od, plans, purpose = "work", folder = ".", batch = TRUE, b
       if (batch && !batch_save) {
         # One-off saving of pre-computed routes:
         id = NULL
-        if (as.character(Sys.time()) < "2024-05-22 08:30:12.486921" && plan == "quietest") {
-          id = 9876
+        if (as.character(Sys.time()) < "2024-05-22 19:55:12.486921" && plan == "fastest") {
+          id = 9896
         }
         routes_raw = cyclestreets::batch(
           id = id,
