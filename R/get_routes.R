54a5329ef972980100b09d1780ff341277353deb
robinlovelace <rob00x@gmail.com>
2024-05-22 09:05:13 +0100

    Use downloaded gz file, close #482

--- R/get_routes.R
+++ R/get_routes.R
@@ -1,32 +1,42 @@
-get_routes = function(od, plans, purpose = "work", folder = ".", batch = TRUE, batch_save = FALSE, nrow_batch = 100, date = NULL, segments = TRUE) {
+get_routes = function(od, plans, purpose = "work", folder = ".", batch = TRUE, batch_save = FALSE, nrow_batch = 100, date = NULL, segments = TRUE, c2k = c("id", "distances", "quietness", "gradient_smooth")) {
   if (nrow(od) < 50) {
     batch = FALSE
   }
   route_list = sapply(plans, function(x) NULL)
   for (plan in plans) {
     message("Getting the ", plan, " routes for ", purpose, " journeys")
-    file_name = paste0("routes_max_dist_", purpose, "_", plan, ".Rds")
+    file_name = glue::glue("routes_{purpose}_{plan}.csv.gz")
     savename_f = file.path(folder, file_name)
+    browser()
     if (file.exists(savename_f)) {
       message("Routes already exist: reading from file: ", savename_f)
-      routes_filtered = readRDS(savename_f)
+      routes_raw = cyclestreets:::batch_read(savename_f, cols_to_keep = c2k, segments = segments)
+      routes_id_table = table(routes_raw$routes$route_number)
+      routes_id_names = sort(as.numeric(names(routes_id_table)))
+      od$id = as.character(seq(nrow(od)))
+      od = sf::st_drop_geometry(od)
+      n_routes_removed = nrow(od) - length(routes_id_names)
+      message(n_routes_removed, " routes removed")
+      routes_raw$routes = dplyr::left_join(routes_raw$routes, od, by = dplyr::join_by(route_number == id))
     } else {
       if (batch && !batch_save) {
         # One-off saving of pre-computed routes:
         id = NULL
-        if (as.character(Sys.time()) < "2024-05-22 08:30:12.486921" && plan == "quietest") {
-          id = 9876
+        if (as.character(Sys.time()) < "2024-05-22 08:59:12.486921") {
+          id = 9881
         }
         routes_raw = cyclestreets::batch(
-          id = id,
           desire_lines = od,
+          id = id,
+          directory = folder,
           wait = TRUE,
           maxDistance = 30000,
           username = "robinlovelace",
           strategies = plan,
-          cols_to_keep = c("id", "distances", "quietness", "gradient_smooth"),
+          cols_to_keep = c2k,
           segments = segments,
-          delete_job = FALSE
+          delete_job = FALSE,
+          filename = file_name |> gsub(".csv.gz", "", x = _)
         )
       } else {
         if (batch_save) {
