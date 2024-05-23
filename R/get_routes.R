7448b1aa0fbe177bcab50f7a8c52b8382a97717d
robinlovelace <rob00x@gmail.com>
2024-05-23 12:16:26 +0100

    More tidying: remove batch_save

--- R/get_routes.R
+++ R/get_routes.R
@@ -1,4 +1,4 @@
-get_routes = function(od, plans, purpose = "work", folder = ".", batch = TRUE, batch_save = FALSE, nrow_batch = 100, date = NULL, segments = TRUE, c2k = c("id", "distances", "quietness", "gradient_smooth")) {
+get_routes = function(od, plans, purpose = "work", folder = ".", batch = TRUE, nrow_batch = 100, date = NULL, segments = TRUE, c2k = c("id", "distances", "quietness", "gradient_smooth")) {
   if (nrow(od) < 50) {
     batch = FALSE
   }
@@ -61,96 +61,6 @@ get_routes = function(od, plans, purpose = "work", folder = ".", batch = TRUE, b
     route_list[[paste(plan)]] = routes_filtered
   }
   route_list
-}
-
-#' Save routes in batches before returning the result
-#'
-#' An alternative to routing functions
-#'
-#' @examples
-# tar_load(od_commute_subset)
-# od = od_commute_subset
-# fun = stplanr::route
-# nrow_batch = 10
-# plan = "fastest"
-# purpose = "work"
-batch_routes = function(od, fun, nrow_batch = 100, plan = "fastest", purpose, ..., temp_folder = tempdir()) {
-  nrow_od = nrow(od)
-  # Split up into list
-  od$splittingID = ceiling(seq_len(nrow(od)) / nrow_batch)
-  od = dplyr::group_by(od, splittingID)
-  od = dplyr::group_split(od)
-
-  max_pad = nchar(as.character(length(od)))
-
-  results = list()
-  for (i in seq_len(length(od))) {
-    od_to_route = od[[i]]
-    id = stringr::str_pad(i, max_pad, pad = "0")
-    f = paste0("batch_", plan, "_", purpose, "_", id, "_with_", nrow(od_to_route), "_routes_of_", nrow_od, "_rows.Rds")
-    f = file.path(temp_folder, f)
-    message(Sys.time(), " doing batch ", id, " of ", length(od))
-    message("Number of rows in batch: ", nrow(od_to_route))
-    message("Looking in the file: ", f)
-
-    # results[[i]] <- fun(od_to_route, ...)
-    if (file.exists(f)) {
-      message("File exists")
-      results[[i]] = readRDS(f)
-    } else {
-      message("File does not exist")
-      # Retry failing batches
-      # Source: https://stackoverflow.com/questions/31999808/retry-for-loop-r-loop-if-error
-      # results = as.list(1:100) # for testing
-      while (TRUE) {
-        message("Starting routing")
-
-        id_batch = NULL
-
-        # Recover routes:
-        # if(plan == "balanced") {
-        #   id_batch = as.numeric(id) + 3809 - 10
-        #   message("id ", id_batch, " for ", id)
-        # } else {
-        #   id_batch = NULL
-        # }
-
-        results[[i]] = try(
-
-          # standard routing:
-          # fun(
-          #   l = od_to_route,
-          #   route_fun = cyclestreets::journey,
-          #   plan = plan,
-          #   warnNA = FALSE
-          #   # comment-out this line to use default instance:
-          #   # base_url = "http://5b44de2e26338760-api.cyclestreets.net",
-          #   # pat = Sys.getenv("CYCLESTREETS_BATCH")
-          # )
-          # batch routing:
-          cyclestreets::batch(
-            desire_lines = od_to_route,
-            id = id_batch,
-            maxDistance = 30000,
-            username = "robinlovelace",
-            strategies = plan,
-            wait = TRUE
-          )
-        )
-        if (!is(results[[i]], "try-error")) break
-      }
-      message("Saving ", f, " to ", temp_folder)
-      saveRDS(results[[i]], f)
-      f = paste0("od", "_", purpose, "_", id, "_with_", nrow(od_to_route), "_of_", nrow_od, "_rows.Rds")
-      f = file.path(temp_folder, f)
-      message("Saving ", f, " to ", temp_folder)
-      saveRDS(od_to_route, f)
-    }
-  }
-  message("Combining results")
-  saveRDS(results, file.path(temp_folder, "results_list.Rds"))
-  result = bind_sf(results)
-  return(result)
 }
 
 bind_sf = function(x) {
