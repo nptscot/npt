f831e2932cbe2a44efafc15138dc7180c83c9841
robinlovelace <rob00x@gmail.com>
2024-06-20 14:36:38 +0100

    Debug error

--- _targets.R
+++ _targets.R
@@ -789,6 +789,13 @@ list(
     od_visiting
   }),
   tar_target(od_leisure, {
+    tar_load(oas)
+    tar_load(os_pois)
+    tar_load(grid)
+    tar_load(trip_purposes)
+    tar_load(zones)
+    tar_load(parameters)
+    tar_source()
     od_leisure = make_od(
       oas, os_pois, grid,
       purpose = "leisure",
