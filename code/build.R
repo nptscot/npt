9599325d27de754bd5e00faac3f59ab03796a097
robinlovelace <rob00x@gmail.com>
2024-09-04 15:20:01 +0000

    Comment out working directory code

--- code/build.R
+++ code/build.R
@@ -772,8 +772,9 @@ output_folders = list.dirs(output_folder)[-1]
 regional_output_files = list.files(output_folders[1])
 regional_output_files
 
-# set working directory
-setwd("/workspaces/npt")
+# # TODO: Remove this?
+# # set working directory
+# setwd("/workspaces/npt")
 
 # Combine regional route networks:
 combined_network_list = lapply(output_folders, function(folder) {
